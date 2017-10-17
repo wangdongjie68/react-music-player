7bc4f56df5424d3b62c863960df683fee1baae4b
wangdongjie <wdj68@sina.com>
2017-10-17 14:04:56 +0800

    add loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,25 @@ module.exports = {
     output: {
         path: path.resolve(__dirname + '/dist'),
         filename: 'bundle.js'
-    },
+     },
+    module:{
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets:['react','es2015']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.less/,
+                loader: 'style-loader!css-loader!less-loader'
+            }
+        ]
+    }
 }
