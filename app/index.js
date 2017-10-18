cfb2aeb5f3c6b6399c06654af9c82907f75189e4
wangdongjie <wdj68@sina.com>
2017-10-18 21:32:40 +0800

    修改依赖包。

--- app/index.js
+++ app/index.js
@@ -1,4 +1,8 @@
 import React from 'react'
-import './index.less'
+import { render } from 'react-dom';
+import Hello from './components/hello'
 
-console.log(React.version);
+render(
+    <Hello></Hello>,
+    document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,52 @@
-const path = require('path');
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './app/index.js',
+    devtool: 'eval-source-map',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        'react-hot-loader/patch',
+        path.join(__dirname, 'app/index.js')
+    ],
     output: {
-        path: path.resolve(__dirname + '/dist'),
-        filename: 'bundle.js'
-     },
-    module:{
+        path: path.join(__dirname, '/dist/'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './app/index.tpl.html',
+          inject: 'body',
+          filename: './index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
+    module: {
+        resolve:{
+            extensions:['','.js','.json']
+        },        
         loaders: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                query: {
-                    presets:['react','es2015']
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: "babel-loader",
+              query:
+                {
+                  presets:['react','es2015']
                 }
+            },
+            {
+                test: /\.json?$/,
+                loader: 'json'
             },
             {
                 test: /\.css$/,
@@ -26,4 +58,4 @@ module.exports = {
             }
         ]
     }
-}
+};
