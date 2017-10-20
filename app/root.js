3d40807e55dc66933df6db8295c56ee294f5480d
wangdongjie <wdj68@sina.com>
2017-10-20 20:11:39 +0800

    实现音乐的播放

--- app/root.js
+++ app/root.js
@@ -3,11 +3,34 @@ import Header from './components/header'
 import Progress from './components/progress'
 
 let Root = React.createClass({
+    getInitialState(){
+        return{
+            progress: '-'
+        }
+    },
+
+    componentDidMount(){
+        $('#player').jPlayer({
+            ready: function(){
+                $(this).jPlayer('setMedia',{
+                    mp3:'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
+                }).jPlayer('play');
+            },
+            supplied: 'mp3',
+            wmode: 'window'
+        });
+        $('#player').bind($.jPlayer.event.timeupdate,(e)=>{
+            this.setState({
+                progress: Math.round(e.jPlayer.status.currentTime)
+            });
+        });
+    },
+
     render(){
         return(
             <div>
             <Header />
-            <Progress progress="6">
+            <Progress progress={this.state.progress}>
             </Progress>
             </div>
         );
